910be66a8a1abdccfb6c80de7318ee5e311d525e
Ålexander <yurchenko@post.com>
2021-11-03 13:58:20 +0100

    Added a file with phones data

---
+++ src/PhonesData/TextData/ModelNames.js
@@ -0,0 +1,27 @@
+const models = [
+  {
+    year: 1998,
+    name: "C11",
+    body: "137 x 55 x 22 mm",
+    display: {
+      type: "Alphanumeric",
+    },
+  },
+  {
+    year: 1998,
+    name: "C10",
+    body: "137 x 55 x 22 mm",
+    display: {
+      type: "Alphanumeric",
+    },
+  },
+  {
+    year: 1998,
+    name: "S11",
+    body: "147 x 46 x 25 mm",
+    display: {
+      type: "Color graphic",
+      resolution: "97 x 54 pixels",
+    },
+  },
+];
